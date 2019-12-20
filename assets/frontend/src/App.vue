f8dec2275242774e51623f354465a71378703c5d
Meelouth <justtelejka@yandex.ru>
2019-12-20 21:14:13 +0300

    Main page vue

--- assets/frontend/src/App.vue
+++ assets/frontend/src/App.vue
@@ -55,10 +55,6 @@ Ads">Interest-Based Ads</a><span>  |  </span><a href="#" target="_blank"
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  
 }
 </style>
